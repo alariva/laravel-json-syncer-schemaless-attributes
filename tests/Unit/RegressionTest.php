028b9aa439cd600abaf8fbeabe1be08501498f0f
Ariel Vallese <alariva@gmail.com>
2018-09-19 18:23:58 -0300

    Add test for JSON export

--- tests/Unit/RegressionTest.php
+++ tests/Unit/RegressionTest.php
@@ -32,4 +32,26 @@ class RegressionTest extends TestCase
         $this->assertTrue($user->settings->die_hard);
         $this->assertEquals($user->settings->cups_of_coffee_per_day, 17);
     }
+
+    public function test_it_can_json_export_a_model_with_schemaless_attributes()
+    {
+        $user = User::create(['email' => 'example@example.com', 'name' => 'Test User', 'password' => bcrypt('test')]);
+
+        $user->settings->hero_mode = true;
+        $user->settings->die_hard = true;
+        $user->settings->cups_of_coffee_per_day = 17;
+
+        $user->save();
+
+        $user->refresh();
+
+        $export = $user->exportToJson();
+
+        $jsonObject = json_decode($export);
+
+        $this->assertInternalType('string', $export);
+        $this->assertTrue($jsonObject->settings->hero_mode);
+        $this->assertTrue($jsonObject->settings->die_hard);
+        $this->assertEquals($jsonObject->settings->cups_of_coffee_per_day, 17);
+    }
 }
